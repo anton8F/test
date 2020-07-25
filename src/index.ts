64702154133362f2d857a6ee1272b55cf481509f
anton8F <feleleg@mail.ru>
2020-07-25 23:59:16 +0700

    new problem

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
+import "reflect-metadata";
+import {createConnection, getRepository} from "typeorm";
+import {User} from "./entities/User";
+import {Post} from "./entities/Post";
+import {Comment} from "./entities/Comment";
+
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+async function connect(){
+    await createConnection();
+}
+
+connect();
 
 app.route('/posts')
-  .get(function(req, res) {
-    res.send('hello');
+  .get(async function(req, res) {
+    res.json(await User.findOne());
   })
-  .post(function(req, res) {
-    res.send('bye');
+  .post(urlencodedParser, async function(req, res) {
+    res.json(req.body);
   });
 
 app.route('/posts/:id')
